97b3517879b8f0631a0a84e7988517470b987b97
RAMESH KUMAR THAKUR <89151608+RTSIR@users.noreply.github.com>
2022-02-17 17:56:01 +0530

    Update Test_SAR.py

--- Test_SAR.py
+++ Test_SAR.py
@@ -34,7 +34,7 @@ def my_Vfilter(shape, dtype=None):
 #ParsingArguments
 parser=argparse.ArgumentParser()
 parser.add_argument('--dataPath',dest='dataPath',type=str,default='./Testing_data/SAR',help='testDataPath')
-parser.add_argument('--weightsPath',dest='weightsPath',type=str,default='./Pretrained_models/AGSDNet_SAR.h5',help='pathOfTrainedCNN')
+parser.add_argument('--weightsPath',dest='weightsPath',type=str,default='./Pretrained_models/AGSDNet.h5',help='pathOfTrainedCNN')
 args=parser.parse_args()
 #createModel, loadWeights
 def custom_loss(y_true,y_pred): #this is required for loading a keras-model created with custom-loss
